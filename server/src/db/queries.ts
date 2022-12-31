d446c3317868b1a4828a03e9cc19ec3089aed02b
muusitalo <mUusitalo@users.noreply.github.com>
2022-12-31 21:28:36 +0200

    Move queries to single file

--- server/src/db/queries.ts
+++ server/src/db/queries.ts
@@ -1,4 +1,6 @@
-import prisma from './client'
+import { Prisma, PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 export async function deleteOldPositions(deleteOlderThanMilliSeconds: number) {
   return prisma.position.deleteMany({
@@ -17,5 +19,19 @@ export async function getDronePaths() {
         orderBy: { timestamp: 'asc' },
       },
     },
+  })
+}
+
+export async function createDrones(drones: Prisma.DroneCreateManyInput[]) {
+  return await prisma.drone.createMany({
+    data: drones,
+  })
+}
+
+export async function createPositions(
+  positions: Prisma.PositionCreateManyInput[]
+) {
+  return await prisma.position.createMany({
+    data: positions,
   })
 }
