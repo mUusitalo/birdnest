0d5dda2bdab4e0be141d01aaa6910153a8a7a782
muusitalo <mUusitalo@users.noreply.github.com>
2023-01-01 02:03:38 +0200

    Remove NDZ violators after 10 minutes

--- minimalist/src/getUpdatedViolators.js
+++ minimalist/src/getUpdatedViolators.js
@@ -1,6 +1,7 @@
 import { updateExistingViolator } from "./updateViolator.js";
 import { calculatePartitionedViolators } from './calculatePartitionedViolators.js';
 import { mergePilotInfoWithDrone } from "./mergePilotInfoWithDrone.js";
+import { removeOldViolators } from "./removeOldViolators.js";
 
 export async function getUpdatedViolators(drones, timestamp, currentViolators) {
   const updatedViolators = [...currentViolators]
@@ -10,9 +11,8 @@ export async function getUpdatedViolators(drones, timestamp, currentViolators) {
   existingViolators.forEach(existingViolator => updateExistingViolator(currentViolators, existingViolator, timestamp));
   const promises = newViolators.map(newViolator => mergePilotInfoWithDrone(newViolator, timestamp));
   const mergedViolators = await Promise.all(promises)
-  
-  console.log(mergedViolators)
   updatedViolators.push(...mergedViolators)
+  const oldViolatorsRemoved = removeOldViolators(updatedViolators, timestamp)
   
-  return updatedViolators
+  return oldViolatorsRemoved
 }
