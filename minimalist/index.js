8f6f15ef6c15409685d6e6d42fd3c36d4c943397
muusitalo <mUusitalo@users.noreply.github.com>
2023-01-01 19:24:32 +0200

    Display list in frontend

--- minimalist/index.js
+++ minimalist/index.js
@@ -1,6 +1,8 @@
 import { fetchRadarData } from "./src/fetching/fetchRadarData.js"
 import { getUpdatedViolators } from './src/getUpdatedViolators.js'
-import { UPDATE_INTERVAL_MILLISECONDS } from './src/utils/config.js'
+import { PORT, UPDATE_INTERVAL_MILLISECONDS } from './src/utils/config.js'
+import fs from 'fs/promises'
+import http from 'http'
 
 var currentViolators = []
 
@@ -10,5 +12,27 @@ async function update() {
   console.log(currentViolators)
 }
 
+http.createServer(async (req, res) => {
+  const { url } = req
+  switch(url) {
+    case "/":
+      res.writeHead(200, {'Content-Type': 'text/html'})
+      // Reading the file every time for easier debugging
+      res.write(await fs.readFile('index.html'))
+      res.end()
+      break
+    case "/api/violators":
+      const json = JSON.stringify(currentViolators)
+      res.writeHead(200, {'Content-Type': 'application/json', 'Content-Length': Buffer.byteLength(json)})
+      res.write(JSON.stringify(currentViolators))
+      res.end()
+      break
+    default:
+      res.end('Not found')
+      break
+  }
+}).listen(PORT)
+
+update()
 setInterval(update, UPDATE_INTERVAL_MILLISECONDS)
 
