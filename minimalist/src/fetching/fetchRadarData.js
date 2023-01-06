27b1e3cdeec0ec003014db0ab002e3552c9af35e
muusitalo <mUusitalo@users.noreply.github.com>
2023-01-06 02:53:55 +0200

    Use empty list if no drones were found

--- minimalist/src/fetching/fetchRadarData.js
+++ minimalist/src/fetching/fetchRadarData.js
@@ -4,5 +4,5 @@ import parser from "../utils/parser.js";
 export async function fetchRadarData() {
   const response = await fetch(DRONE_API_URL);
   const { report } = parser.parse(await response.text());
-  return [report.capture.drone, new Date(report.capture['@_snapshotTimestamp'])];
+  return [report.capture.drone ?? [], new Date(report.capture['@_snapshotTimestamp'])];
 }
