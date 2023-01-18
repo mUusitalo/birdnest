0860080999b7f8e687083a51f9bb67596322e308
mUusitalo <73437699+mUusitalo@users.noreply.github.com>
2023-01-18 22:12:01 +0200

    Fetch data less frequently to avoid rate limits

--- src/utils/config.js
+++ src/utils/config.js
@@ -4,11 +4,11 @@ const NDZ_RADIUS = 100
 const NDZ_CENTER = { x: 250, y: 250 }
 
 /**
- * Updating every 500 milliseconds to decrease the probability of missing an update from the server that updates around every 2 seconds.
+ * Updating every 1000 milliseconds to decrease the probability of missing an update from the server that updates around every 2 seconds.
  * Because the supplied endpoint is a simple HTTP endpoint that responds immediately (thus doesn't support long polling),
  * this is the best we can do without spamming the server with a bunch of requests.
  */
-const UPDATE_INTERVAL_MILLISECONDS = 500
+const UPDATE_INTERVAL_MILLISECONDS = 1000
 const VIOLATOR_PERSISTENCE_MILLISECONDS = 60_000 * 10
 const PORT = 8080
 
